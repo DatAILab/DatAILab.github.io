e02c0056f3f26746271660d916a50492a1c302b8
DatAILab <bayram.hamrouni@datailab.onmicrosoft.com>
2024-10-14 16:11:33 +0100

    Add files via upload

--- QuestionGenerator.py
+++ QuestionGenerator.py
@@ -1,14 +1,13 @@
 import streamlit as st
-from google.cloud import firestore
-from google.oauth2 import service_account
-from retry import retry
+import firebase_admin
+from firebase_admin import credentials, firestore
 
 # Firebase initialization (outside Streamlit components)
 def initialize_firebase():
-    if 'db' not in st.session_state:  # Check if Firestore client is already initialized
-        print("Initializing Firestore...")
+    if not firebase_admin._apps:  # Check if any Firebase app is already initialized
+        print("Initializing Firebase...")
         # Use the credentials from the secrets
-        credentials = service_account.Credentials.from_service_account_info({
+        cred = credentials.Certificate({
             "type": st.secrets["type"],
             "project_id": st.secrets["project_id"],
             "private_key_id": st.secrets["private_key_id"],
@@ -21,14 +20,13 @@ def initialize_firebase():
             "client_x509_cert_url": st.secrets["client_x509_cert_url"],
             "universe_domain": st.secrets["universe_domain"]
         })
-        st.session_state.db = firestore.Client(credentials=credentials)
+        firebase_admin.initialize_app(cred)
     else:
-        print("Firestore is already initialized.")
+        print("Firebase is already initialized.")
 
-@retry(exceptions=Exception, tries=3, delay=5, backoff=2, jitter=(1, 3))
 def fetch_all_questions():
     try:
-        db = st.session_state.db
+        db = firestore.client()
         questions_ref = db.collection("questions")
         query_snapshot = questions_ref.get()
 
@@ -41,10 +39,10 @@ def fetch_all_questions():
         return questions
     except Exception as e:
         st.error(f"Error retrieving questions: {e}")
-        raise  # Re-raise the exception to trigger the retry
+        return []  # Return an empty list on error
 
 def main():
-    # Initialize Firestore
+    # Initialize Firebase
     initialize_firebase()
 
     st.title("Quiz Application")
