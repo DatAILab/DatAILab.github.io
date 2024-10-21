abde07e74170cf9ff62dc589cb93f31e5374b007
DatAILab <bayram.hamrouni@datailab.onmicrosoft.com>
2024-10-21 20:42:20 +0100

    Add files via upload

--- QuestionGenerator.py
+++ QuestionGenerator.py
@@ -82,7 +82,9 @@ if st.button("Soumettre"):
     total_questions = len(questions)
     correct_percentage = (correct_count / total_questions) * 100
 
-    # Create a gauge chart with a value of 70
+    
+
+    # Create a gauge chart
     gauge_fig = go.Figure(go.Indicator(
         mode="gauge+number",
         value=correct_percentage,
@@ -94,11 +96,6 @@ if st.button("Soumettre"):
                 {'range': [0, 69], 'color': "red"},
                 {'range': [70, 100], 'color': "lightgreen"},
             ],
-            'threshold': {
-                'line': {'color': "blue", 'width': 4},
-                'thickness': 0.75,
-                'value': 70  # This sets the threshold line at 70
-            }
         }
     ))
 
