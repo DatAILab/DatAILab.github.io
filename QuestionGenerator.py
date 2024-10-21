bc4975fdd9fc9396c50ae4acb5462fdbf4e2d99c
DatAILab <bayram.hamrouni@datailab.onmicrosoft.com>
2024-10-21 20:32:40 +0100

    Add files via upload

--- QuestionGenerator.py
+++ QuestionGenerator.py
@@ -92,7 +92,7 @@ if st.button("Soumettre"):
             'bar': {'color': "white"},
             'steps': [
                 {'range': [0, 69], 'color': "red"},
-                {'range': [70, 100], 'color': "lightgreen"},
+                {'range': [70, 100], 'color': "blue"},
             ],
             'threshold': {
                 'line': {'color': "blue", 'width': 4},
